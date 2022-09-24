104f7625023af6841e76d73d04e3fe09d29bab8c
Denis Gorlov <dengorl1998@gmail.com>
2022-09-24 21:34:25 +0300

    minor changes fot better code reading in GameEven.java

--- src/main/java/hexlet.code/GameEven.java
+++ src/main/java/hexlet.code/GameEven.java
@@ -5,36 +5,43 @@ import java.util.Scanner;
 
 public class GameEven {
     public static void playingGameEven() {
-        var ourPlayer = new Cli();
-        ourPlayer.greetPlayer();
+        var player = new Cli();
+        player.greetPlayer();
 
         int bottomBorder = 1;
         int upperBorder = 50;
         int diff = upperBorder - bottomBorder;
+        String correctAnswer = "'yes'";
+        String incorrectAnswer = "'no'";
 
         for (int i = 0; i <= 2; i++) {
             Random rn = new Random();
             int number = rn.nextInt(diff + 1) + bottomBorder;
             int parity = number % 2;
 
+            if (parity == 1) {
+                correctAnswer = "'no'";
+                incorrectAnswer = "'yes'";
+            }
+
             System.out.print("Answer 'yes' if the number is even, otherwise answer 'no'."
                     + "\nQuestion: " + number + "\nYour answer: ");
-            String correctAnswer = "Correct!";
-            String incorrectAnswer = "'yes' is wrong answer ;(. Correct answer was 'no'."
-                    + "\nLet's try again, " + ourPlayer.getPlayerName() + "!";
 
             Scanner scan = new Scanner(System.in);
             String answer = scan.nextLine();
 
+            String incorrectMessage = incorrectAnswer +" is wrong answer ;(. Correct answer was " + correctAnswer + "."
+                    + "\nLet's try again, " + player.getPlayerName() + "!";
+
             String message = (((parity == 0) && (answer.equals("yes")))
-                    || ((parity == 1) && (answer.equals("no")))) ? correctAnswer : incorrectAnswer;
+                    || ((parity == 1) && (answer.equals("no")))) ? "Correct!" : incorrectMessage;
 
             System.out.println(message);
 
-            if (message.equals(incorrectAnswer)) {
+            if (message.equals(incorrectMessage)) {
                 return;
             }
         }
-        System.out.println("Congratulations, " + ourPlayer.getPlayerName() + "!");
+        System.out.println("Congratulations, " + player.getPlayerName() + "!");
     }
 }
