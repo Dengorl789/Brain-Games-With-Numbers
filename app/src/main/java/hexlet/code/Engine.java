82b27bf4474c7faa3d2f541772173eceb82f50c7
Denis Gorlov <dengorl1998@gmail.com>
2022-11-04 19:50:49 +0300

    moving array creating back to games and added construction for question message

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,14 +3,11 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Engine {
-    private static final int FIRST_ARRAY_SIZE = 3;
-    private static final int SECOND_ARRAY_SIZE = 2;
+    public static final int FIRST_ARRAY_SIZE = 3;
+    public static final int SECOND_ARRAY_SIZE = 2;
     private static final int NUMBER_OF_ROUNDS = 3;
     private static final String GREETING_MESSAGE = "Welcome to the Brain Games!\nMay I have your name? ";
-    public static String[][] createArrayForQuestionAndAnswerPairs() {
-        return new String[FIRST_ARRAY_SIZE][SECOND_ARRAY_SIZE];
-    }
-    public static void mainEngineMethod(String generalQuestion, String[][] questionAndAnswerPair) {
+    public static void runGame(String generalQuestion, String[][] questionAndAnswerPair) {
         System.out.print(GREETING_MESSAGE);
         Scanner sc = new Scanner(System.in);
         String playerName = sc.nextLine().trim();
@@ -18,13 +15,13 @@ public class Engine {
         System.out.println(generalQuestion);
 
         for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
-            String question = questionAndAnswerPair[i][0];
-            String correctAnswer = questionAndAnswerPair[i][1];
+            String question = "Question: " + questionAndAnswerPair[i][0] + "\nYour answer: ";
             System.out.print(question);
 
             Scanner scan = new Scanner(System.in);
             String answer = scan.nextLine();
 
+            String correctAnswer = questionAndAnswerPair[i][1];
             String correctMessage = "Correct!";
             String incorrectMessage = "'" + answer + "' is wrong answer ;(. Correct answer was "
                     + "'" + correctAnswer + "'." + "\nLet's try again, " + playerName + "!";
